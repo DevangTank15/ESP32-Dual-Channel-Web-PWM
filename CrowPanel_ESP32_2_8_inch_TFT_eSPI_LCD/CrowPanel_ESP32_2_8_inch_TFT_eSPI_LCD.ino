b1f1a61725c000cd5f13b968d4e20b2ade05c234
DevangTank15 <18218419+DevangTank15@users.noreply.github.com>
2026-03-18 15:16:21 +0530

    update latest changes

--- CrowPanel_ESP32_2_8_inch_TFT_eSPI_LCD/CrowPanel_ESP32_2_8_inch_TFT_eSPI_LCD.ino
+++ CrowPanel_ESP32_2_8_inch_TFT_eSPI_LCD/CrowPanel_ESP32_2_8_inch_TFT_eSPI_LCD.ino
@@ -1,5 +1,28 @@
+#include <WiFi.h>
+const char* ssid = "Devang1";
+const char* password = "my@android@12345";
+String serverIP = "http://192.168.4.1";
+void connect_wifi()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect(true);
+    delay(1000);
+    WiFi.begin(ssid,password);
+
+    while(WiFi.status()!=WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("WiFi connected");
+}
+
 void setup()
 {
+    Serial.begin(115200);
+
+    connect_wifi();
 }
 
 void loop()
